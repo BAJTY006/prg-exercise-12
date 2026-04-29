1bb4a6c57675dca9be5e7efad4714f4001e91f91
Vaclav Bajtek <283148@vutbr.cz>
2026-04-29 10:16:58 +0200

    Add numpy

--- main.py
+++ main.py
@@ -5,7 +5,21 @@ def main():
 # když je na konci  kodu v jupiteru jenom proměnná tak se automaticky vypíše jjejí obsah
 # pomocí vykřičníku dávám příkazy
 #i po smazání buňky v které jsem definoval proměnnou, tak si to stále drží v paměti
-#
+#np. array vytváří ze saznamu pole
+#arange je podle nějakého rozsahu range
+#np.linspace
+# zeros vytáří pole kde jsou samé nuly
+#ones vytváří pole kde jsou samé jedničky
+# pro vícerozměrné pole se používají jako první řádky a jako druhé sloupce
+# u matice se když chci k nějaké hodnotě pomocí indexování, rak to píšu do jednich hranatých závorek za sebe
+# pro jeden řádek [2::] asi nějakl tak, prostě vezmu celý řádek
+#maska má v sobě uložené hodnoty true a false
+#když dám podmínku u masky, tak mi to udělá masku kde bude True až tam kde je splěnná ta podmínka.
+# nebo mohu napsat podmínku přimo když to chci vybrat
+#akorát je potřeba používat speciální znaky místo and a or
+#když dělám nějakou operaci na těch polích,t ak se mit děje ve všech polích naráz
+#sum, mean, max atd se volá jako metoda na tom objektu toho pole
+
 
 if __name__ == "__main__":
     main()
