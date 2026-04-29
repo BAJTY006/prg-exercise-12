fe9ff62a3fb4d2cfa67694a8a1e6d75332aafd91
Vaclav Bajtek <283148@vutbr.cz>
2026-04-29 09:10:57 +0200

    Add notebook

---
+++ main.py
@@ -0,0 +1,11 @@
+def main():
+    print("Hello from prg-exercise-12!")
+
+
+# když je na konci  kodu v jupiteru jenom proměnná tak se automaticky vypíše jjejí obsah
+# pomocí vykřičníku dávám příkazy
+#i po smazání buňky v které jsem definoval proměnnou, tak si to stále drží v paměti
+#
+
+if __name__ == "__main__":
+    main()
